ab83769fd52d36ab443958de44014a7a104f96e1
DashMiner <dashminerpool@gmail.com>
2015-06-25 18:09:01 +1200

    DashMiner

---
+++ example.bat
@@ -0,0 +1,6 @@
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+sgminer.exe -o sf -u XpAy7r5RVdGLnnjWNKuB9EUDiJ5Tje9GZ8_test -p YourEmail@somesite.com -I 20
+
+pause
